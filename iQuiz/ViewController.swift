97e71fd0f9878b23adf2183e27152c827b40cd75
wycwyc99a88b <wyc@d-69-91-151-140.dhcp4.washington.edu>
2017-05-02 14:56:02 -0700

    2

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -21,10 +21,15 @@ class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.tableFooterView = UIView()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return subjects.count
     }
