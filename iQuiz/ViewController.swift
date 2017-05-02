a624368cf9c61a5bb6587d9fe57bde6391dd5722
wycwyc99a88b <wyc@wise.local>
2017-05-01 18:51:51 -0700

    1

---
+++ iQuiz/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by Yichao Wang on 5/1/17.
+//  Copyright © 2017 Yichao Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    let subjects = ["Mathematics", "Marvel Super Heroes", "Science"]
+
+
+    @IBAction func settingPressed(_ sender: Any) {
+        let alert = UIAlertController(title: "Setting", message: "Settings go here", preferredStyle:UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return subjects.count
+    }
+
+      
+    override func tableView(_ cellForRowAttableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        cell.textLabel!.text = subjects[indexPath.row]
+        cell.detailTextLabel!.text = "description"
+        cell.imageView!.image = UIImage(named: "black-dot")
+        return cell
+      
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
